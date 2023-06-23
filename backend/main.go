f579b784b65aaa874d8faf9cd9a5cb991d286886
Marcus Sanchez <marcussanchez031@gmail.com>
2023-06-22 21:55:23 -0400

    minimal adjustments

--- backend/main.go
+++ backend/main.go
@@ -5,7 +5,7 @@ import (
 	"WSChatRooms/router"
 
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	recover2 "github.com/gofiber/fiber/v2/middleware/recover"
+	recovery "github.com/gofiber/fiber/v2/middleware/recover"
 
 	"github.com/gofiber/fiber/v2"
 
@@ -30,7 +30,7 @@ func initFiber() *fiber.App {
 	app.Static("/", "./public")
 	app.Use("/ws", isWebsocket.New())
 	app.Use(cors.New())
-	app.Use(recover2.New())
+	app.Use(recovery.New())
 	return app
 }
 
